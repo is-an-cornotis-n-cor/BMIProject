19aa4294d208c2761102259083c8366017c5bed0
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-19 09:25:42 -0400

    Added "inches*0.0254=meters" to main method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,14 +9,16 @@ public class Main {
     public static void main(String[] args) {
     //declare
     Scanner keyboard;
-    int inches, meters,bmi,kg;
+        int inches,bmi,kg,pounds;
+        double CONVERSION_IN_METERS =  0.0254, meters;
 
     //initializing
      keyboard = new Scanner (System.in);
 
      System.out.println("What is your height in inches");
-     inches = keyboard.nextLine();
-     meters = inches * 0.0254;
+     inches = keyboard.nextInt();
+        meters=inches*0.0254;
+     System.out.println();
      System.out.println("What is your weight in pounds");
 
 
