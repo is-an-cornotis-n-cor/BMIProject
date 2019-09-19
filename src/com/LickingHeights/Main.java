8204b5b6d57c95a9eba201a671d002394d062593
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-19 09:09:03 -0400

    intial commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,25 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+    //declare
+    Scanner keyboard;
+    int inches, meters,bmi,kg;
+
+    //initializing
+     keyboard = new Scanner (System.in);
+
+     System.out.println("What is your height in inches");
+     inches = keyboard.nextLine();
+     meters = inches * 0.0254;
+     System.out.println("What is your weight in pounds");
+
+
+
+    }
+}
